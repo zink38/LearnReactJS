a7e8b15e8020c066a4117a50f47d2b4a84b053c5
Keegan Churchill <keeganbchurchill@gmail.com>
2021-05-14 16:57:30 -0230

    Lifting Up state 1

--- src/index.js
+++ src/index.js
@@ -2,19 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+  class Square extends React.Component {
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button 
+          className="square" 
+          onClick={() => this.props.onClick()}
+        >
+          {this.props.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+    }
     renderSquare(i) {
-      return <Square />;
+      return ( 
+        <Square 
+          value={this.state.squares[i]} 
+          onClick={() => this.handleClick(i)}
+        />
+      );
     }
   
     render() {
